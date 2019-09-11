6ff84a4399d0256527c1cd8de91c041364cc1f6d
MorsChen <MorsChenArtist@users.noreply.github.com>
2019-09-11 12:55:59 +0700

    add migration

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ POSTGRES = {
     'host': "localhost",
     'port': 5432,
 }
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:\
-%(port)s/%(db)s' % POSTGRES
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:\
+# %(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
